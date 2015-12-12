eb7e390533cdd783bf5645148a9808fd90fd7d2d
Fabian Baier <fabian.baier1987@gmail.com>
2015-12-12 01:50:59 -0800

    changed to ubuntu

--- bumblepi.sh
+++ bumblepi.sh
@@ -30,7 +30,7 @@
 	rm fstab
 	wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/fstab
 	sudo reboot
-	cd /home/pi/bumblepi
+	cd /home/ubuntu/bumblepi
 	echo "`date -u` Error: reboot not possible" >> bumblepi.log
  }
 
@@ -45,21 +45,4 @@ echo "`date -u` USB boot not activated" >> bumblepi.log
 check_usb
 fi
 
-#SDCARD_DEVICE="/dev/mmcblk0"
-#USB_DEVICE="/dev/sda"
-#NEW_SLASHDEV="/dev/mmcblk0p2"
-#NEW_BOOTDEV="/dev/mmcblk0p1"
-#MOUNTPOINT="/tmp/USB"
-
-#parted --script "${USB_DEVICE}"  "mklabel msdos"
-#parted --script "${USB_DEVICE}"  "mkpart primary ext4 1MiB -1s"
-#parted --script "${USB_DEVICE}"  print
-#mkfs.ext4 -F -j "${USB_DEVICE}2" 
-#mkdir -p "${MOUNTPOINT}"
-#mount "${USB_DEVICE}1" "${MOUNTPOINT}"
-#mkdir -p "${MOUNTPOINT}/boot"
-#mount "${SDCARD_DEVICE}1" "${MOUNTPOINT}/boot"
-#df -h "${MOUNTPOINT}"
-#df -h "${MOUNTPOINT}/boot"
-#read -p "Press ENTER To begin image restore"
 #tar $@ -xvzf  "${PI_BACKUP_SLASH}" -C "${MOUNTPOINT}"
