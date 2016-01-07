e35efc62b748d81a92ef001a9a92d2627299e249
Fabian Baier <fabian.baier1987@gmail.com>
2016-01-06 22:27:37 -0800

    Readjusted for raspbian

--- bumblepi.sh
+++ bumblepi.sh
@@ -21,18 +21,19 @@
 	mke2fs -t ext4 -L rootfs /dev/${USB_DEVICE}1
 	mount /dev/${USB_DEVICE}1 /mnt
 	rsync -axv / /mnt
-	# cp /boot/cmdline.txt /boot/cmdline.orig
-	#cd /boot
-	cd /boot/firmware
-	cp cmdline.txt cmdline.orig
+	cp /boot/cmdline.txt /boot/cmdline.orig
+	cd /boot
+	#cd /boot/firmware
+	#cp cmdline.txt cmdline.orig
 	rm -f cmdline.txt
-	#wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/cmdline.txt
-	wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/cmdline-ubuntu.txt
-	mv cmdline-ubuntu.txt cmdline.txt
+	wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/cmdline.txt
+	#wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/cmdline-ubuntu.txt
+	#mv cmdline-ubuntu.txt cmdline.txt
 	cd /mnt/etc
 	cp fstab fstab.orig
 	rm -f fstab
-	wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/fstab-ubuntu
+	#wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/fstab-ubuntu
+	wget https://raw.githubusercontent.com/fabianbaier/bumblepi/master/fstab
 	mv fstab-ubuntu fstab
 	sudo fallocate -l 1G /swapfile
 	sudo chmod 600 /swapfile
@@ -47,8 +48,8 @@
 
 echo "`date -u` Bumble Pi started with IP `hostname -I`" >> /var/log/bumblepi.log
 
-#if grep root=/dev/sda1 /boot/cmdline.txt; 
-if grep root=/dev/sda1 /boot/firmware/cmdline.txt;
+if grep root=/dev/sda1 /boot/cmdline.txt; 
+#if grep root=/dev/sda1 /boot/firmware/cmdline.txt;
 then
 echo "`date -u` USB boot in cmdline.txt activated" >> /var/log/bumblepi.log
 echo "`date -u` Bumble PI goes back to sleep" >> /var/log/bumblepi.log
